4b45274a671298fa0b4c45d034d6c9d5558d1692
Monish Jain <monishjain1008@gmail.com>
2024-12-22 06:33:52 +0530

    Conditional Statements & Switch

---
+++ Main.java
@@ -0,0 +1,112 @@
+// import java.util.*;
+
+// public class Main {
+//     public static void main(String[] args) {
+//       System.out.println("enter no 1 and 2"); 
+//     //   String name = "ton stari";
+//     //   int age = 56; 
+//     //   int b = 10;
+//     //   int a = 26;
+//     //   int diff = a-b;
+//     //   System.out.println(diff);
+//       Scanner sc = new Scanner(System.in);
+//       int a = sc.nextInt();
+//        int b = sc.nextInt();
+//       System.out.println(a);
+//         System.out.println(b);
+//       int sum = a+ b;
+//       System.out.println(sum);
+      
+      
+//   }
+// }
+
+// import java.util.*;
+
+// public class Main {
+//     public static void main(String[] args) {
+//     Scanner sc = new Scanner (System.in);
+//     System.out.println("a is:");
+//         int a = sc.nextInt();
+//     System.out.println("b is:");
+//     int b = sc.nextInt();
+    
+//         if (a==b){
+//             System.out.println("nos are equal");
+//         }
+//         else if(a>b){
+//             System.out.println("a is greater");
+//         }
+//         else {
+//             System.out.println("a is lesser");
+//         }
+//     }    
+// }
+
+
+// import java.util.*;
+
+// public class Main {
+//     public static void main(String[] args) {
+//     Scanner sc = new Scanner (System.in);
+//     System.out.println("a is:");
+//     int a = sc.nextInt();
+    
+//     if (a==1) {
+//         System.out.println("Namaste");
+//     }
+//     else if (a==2) {
+//         System.out.println("Hello");
+//     }
+//     else if (a==3){
+//         System.out.println("Bonjour");
+//     }
+
+    // Switch cases
+    // switch(a){
+    // case 1:{ System.out.println("Hello"); 
+    //         System.out.println("Hi");}
+    // break;
+    // case 2: System.out.println("Namaste");
+    // break;
+    // case 3: System.out.println("Bonjour");
+    // break;
+    // default : System.out.println("Invalid Button");
+    // }
+
+// }
+// }
+// import java.util.*;
+
+// public class Main {
+//     public static void main(String[] args) {
+//     Scanner sc = new Scanner (System.in);
+//     System.out.println("a:");
+//     float a = sc.nextFloat();
+//     System.out.println("b:");
+//     float b = sc.nextFloat();
+//     System.out.println("L is");
+//     int L = sc.nextInt();
+//     float subtraction = a-b;
+//     float division = a/b;
+//     float modulo= a%b;
+//     switch (L){
+//     case 1 :{ float addition = a+b;
+//             System.out.println(addition);}
+//     break;
+//     case 2 : System.out.println(subtraction);
+//     break;
+//     case 3 : System.out.println(division);
+//     break;
+//     case 4 : {float multiply = a*b; 
+//                 System.out.println(multiply);}
+//     break;
+//     case 5 : System.out.println(modulo);
+//     break;
+// }
+// }
+// }
+
+
+
+
