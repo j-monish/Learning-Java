4e4dbe8d5d8004e7edd244e357e16f2496159b32
Monish Jain <monishjain1008@gmail.com>
2024-12-28 06:07:29 +0530

    OOPS

---
+++ src/OOPS.java
@@ -0,0 +1,30 @@
+// class Student{
+//    String name;
+//     int roll_no;
+    
+//     static void printing(){
+//         System.out.println("This function works");
+//     }
+
+//     void printName(){
+//         System.out.println(this.name); //This points on object that has called the method.
+//     }
+
+// }
+// public class OOPS{
+//     public static void main(String[] args){
+//     Student student1 = new Student();
+//     student1.name="Mohit";
+//     student1.roll_no = 20;
+//     Student student2 = new Student();
+//     student2.name = "Monish";
+//     student2.roll_no = 25;
+//     student1.printing();
+//     student1.printName();
+
+// }
+// }
+
+
+
+
