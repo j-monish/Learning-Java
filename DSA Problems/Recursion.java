836325f3ad33be7d41d10082f1289df60b661314
Monish Jain <monishjain1008@gmail.com>
2025-01-05 01:40:47 +0530

    Recursion Problems

---
+++ DSA Problems/Recursion.java
@@ -0,0 +1,20 @@
+// Print GFG n times without the loop.
+
+// class Solution {
+//     void helperPrintGfg(int N, int count){
+//         if(count == N+1){
+//             return;
+//         }
+//         // count = 1;
+//         System.out.print("GFG ");
+//         // count = count + 1;
+//         helperPrintGfg(N, count+1);
+//     }
+
+//     void printGfg(int N) {
+//         // code here
+//         int count = 1;
+//         helperPrintGfg(N, count);
+        
+//     }
+// }
