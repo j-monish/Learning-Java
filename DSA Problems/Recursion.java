4a26e7353b18cba7b0f81b9545a660abb4398ee1
Monish Jain <monishjain1008@gmail.com>
2025-01-15 20:01:34 +0530

    Recursion

--- DSA Problems/Recursion.java
+++ DSA Problems/Recursion.java
@@ -18,3 +18,72 @@
         
 //     }
 // }
+
+
+
+
+// Print 1 To N Without Loop
+
+// class Solution {
+    
+//     public void helperPrintNos(int n, int count){
+//         if (count == n+1){
+//             return;
+//         }
+//         System.out.print(count +" ");
+//         count = count + 1;
+//         helperPrintNos(n, count);
+        
+//     }
+
+//     public void printNos(int n) {
+//         // Your code here
+//         int count = 1;
+//         helperPrintNos(n, count);
+//     }
+// }
+
+
+
+// Print N to 1 without loop
+// class Solution {
+//     void helperPrintNos(int N, int count){
+//         if(count == 0 ){
+//             return;
+//         }
+//         System.out.print(count + " ");
+//         helperPrintNos(N, count-1);
+//     }
+
+//     void printNos(int N) {
+//         // code here
+//         int count = N+1;
+//         helperPrintNos(N, count-1);
+//     }
+// }
+
+
+import java.util.*;
+    public class Recursion{
+        public static void helperPrintSum(int sum, int n, int i){
+            if(i == n+1){
+                System.out.println(sum);
+                return;
+            }
+            // sum = i + sum; 
+            // i++; 
+            helperPrintSum(sum+i, n, i+1);
+            
+            }
+            // public static void main(syr){
+            public static void main(String[] args) {
+                int i = 1;
+                int sum =0;
+                Scanner sc = new Scanner(System.in);
+                int n = sc.nextInt();
+                helperPrintSum(sum, n, i);
+                
+            }
+        }
+        
+
